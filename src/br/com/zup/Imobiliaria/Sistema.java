ecef4088e411b53234b7b94c3a41298124ca326e
Michelle Kênia <keniamsoaress@gmail.com>
2021-10-06 11:56:04 -0300

    Criar menu no Sistema

--- src/br/com/zup/Imobiliaria/Sistema.java
+++ src/br/com/zup/Imobiliaria/Sistema.java
@@ -3,9 +3,19 @@ package br.com.zup.Imobiliaria;
 import java.util.Scanner;
 
 public class Sistema {
-    private static Scanner capturarDados (String mensagem) {
+    private static Scanner capturarDados(String mensagem) {
         System.out.println(mensagem);
         return new Scanner(System.in);
     }
+
+    public static void menu() {
+        System.out.println("VOCÊ ESTÁ NO SISTEMA DA IMOBILIÁRIA SOARES");
+        System.out.println("Digite 1 para cadastrar um imóvel");
+        System.out.println("Digite 2 para exibir a lista de imóveis cadastrados");
+        System.out.println("Digite 3 para excluir um morador utilizando seu CPF.");
+        System.out.println("Digite 4 para sair do sistema");
+    }
+}
+
 
 }
