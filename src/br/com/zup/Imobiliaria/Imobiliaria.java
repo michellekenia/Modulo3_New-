325384a3ee52d1d3eb0a0172d83d349b10be2219
Michelle Kênia <keniamsoaress@gmail.com>
2021-10-06 11:06:04 -0300

    Construir classe Imobiliária

--- src/br/com/zup/Imobiliaria/Imobiliaria.java
+++ src/br/com/zup/Imobiliaria/Imobiliaria.java
@@ -1,4 +1,27 @@
 package br.com.zup.Imobiliaria;
 
+import java.util.List;
+
 public class Imobiliaria {
+
+    private List<Imovel> listaDeImoveis;
+
+    public Imobiliaria(List<Imovel> listaDeImoveismoveis) {
+        this.listaDeImoveis = listaDeImoveismoveis;
+    }
+
+    public List<Imovel> getListaDeImoveis() {
+        return listaDeImoveis;
+    }
+
+    public void setListaDeimoveis(List<Imovel> listaDeImoveis) {
+        this.listaDeImoveis = listaDeImoveis;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retorno = new StringBuilder();
+        retorno.append("\n Lista de imóveis :" + listaDeImoveis);
+        return retorno.toString();
+    }
 }
