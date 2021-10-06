d44ded1646d592a736661876b74b02d177af4a2c
Michelle Kênia <keniamsoaress@gmail.com>
2021-10-06 10:40:28 -0300

    Construir classe funcionário

--- src/br/com/zup/Imobiliaria/Funcionario.java
+++ src/br/com/zup/Imobiliaria/Funcionario.java
@@ -1,4 +1,27 @@
 package br.com.zup.Imobiliaria;
 
-public class Funcionario {
+public class Funcionario extends Pessoa {
+
+    private String codigoFuncionario;
+
+    public Funcionario(String nome, String cpf, String codigoFuncionario) {
+        super(nome, cpf);
+        this.codigoFuncionario = codigoFuncionario;
+    }
+
+    public String getCodigoFuncionario() {
+        return codigoFuncionario;
+    }
+
+    public void setCodigoFuncionario(String codigoFuncionario) {
+        this.codigoFuncionario = codigoFuncionario;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retorno = new StringBuilder();
+        retorno.append(super.toString());
+        retorno.append("\n Código do Funcionário :" + codigoFuncionario);
+        return retorno.toString();
+    }
 }
