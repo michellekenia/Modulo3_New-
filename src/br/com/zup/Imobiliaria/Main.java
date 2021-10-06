d7e7a14ca732d7ea05103c4af4978277ba7313e2
Michelle Kênia <keniamsoaress@gmail.com>
2021-10-06 13:45:41 -0300

    Inicir a criação do menu no main

--- src/br/com/zup/Imobiliaria/Main.java
+++ src/br/com/zup/Imobiliaria/Main.java
@@ -1,8 +1,23 @@
 package br.com.zup.Imobiliaria;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //Iniciando testes
+        Scanner leitor = new Scanner(System.in);
+        Imobiliaria imobiliaria = new Imobiliaria();
+
+        int opcaoMenu = 0;
+        boolean menu = true;
+
+        while (menu == true) {
+            System.out.println();
+            opcaoMenu = leitor.nextInt();
+            leitor.nextLine();
+        }
+
     }
 }
