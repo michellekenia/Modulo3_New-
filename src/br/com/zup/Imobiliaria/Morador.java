8dcc915e063c45352c45be5a77d681d41017cbeb
Michelle Kênia <keniamsoaress@gmail.com>
2021-10-06 10:47:33 -0300

    Construir classe Morador

--- src/br/com/zup/Imobiliaria/Morador.java
+++ src/br/com/zup/Imobiliaria/Morador.java
@@ -1,4 +1,39 @@
 package br.com.zup.Imobiliaria;
 
-public class Morador {
+public class Morador extends Pessoa {
+
+    private String telefone;
+    private double renda;
+
+    public Morador(String nome, String cpf, String telefone, double renda) {
+        super(nome, cpf);
+        this.telefone = telefone;
+        this.renda = renda;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public double getRenda() {
+        return renda;
+    }
+
+    public void setRenda(double renda) {
+        this.renda = renda;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retorno = new StringBuilder();
+        retorno.append(super.toString());
+        retorno.append("\n Telefone :" + telefone);
+        retorno.append("\n Renda :" + renda);
+        return retorno.toString();
+
+    }
 }
