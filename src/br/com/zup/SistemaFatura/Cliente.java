47de1cf97e88e9d91a0a1c47788372d8089047bd
Michelle Kênia <keniamsoaress@gmail.com>
2021-10-10 18:11:14 -0300

    Modelar classe Cliente

--- src/br/com/zup/SistemaFatura/Cliente.java
+++ src/br/com/zup/SistemaFatura/Cliente.java
@@ -1,4 +1,48 @@
 package br.com.zup.SistemaFatura;
 
 public class Cliente {
+
+    private String nome;
+    private String cpf;
+    private String email;
+
+    public Cliente(String nome, String cpf, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retorno = new StringBuilder();
+        retorno.append("\nNome: " + nome);
+        retorno.append("\nCpf: " + cpf);
+        retorno.append("\nE-mail: " + email);
+        return retorno.toString();
+    }
 }
+
