20c55ebcb80f08e91b168769a32044513c3104be
Michelle Kênia <keniamsoaress@gmail.com>
2021-10-10 18:38:58 -0300

    Modelar classe Fatura

--- src/br/com/zup/SistemaFatura/Fatura.java
+++ src/br/com/zup/SistemaFatura/Fatura.java
@@ -1,4 +1,49 @@
 package br.com.zup.SistemaFatura;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Fatura {
+    private double valor;
+    private String dataDeVencimento;
+    private Cliente cliente;
+
+    public Fatura(double valor, String dataDeVencimento,Cliente cliente) {
+        this.valor = valor;
+        this.dataDeVencimento = dataDeVencimento;
+        this.cliente = cliente;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getDataDeVencimento() {
+        return dataDeVencimento;
+    }
+
+    public void setDataDeVencimento(String dataDeVencimento) {
+        this.dataDeVencimento = dataDeVencimento;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retorno = new StringBuilder();
+        retorno.append("\nValor da fatura: " + valor);
+        retorno.append("\nData de vencimento: " + dataDeVencimento);
+        retorno.append("\nCliente  " +cliente);
+        return retorno.toString();
+    }
 }
