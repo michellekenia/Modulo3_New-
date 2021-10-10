e770b764a838ef3dd178f52f46fed3aed3eafdb0
Michelle Kênia <keniamsoaress@gmail.com>
2021-10-10 18:59:16 -0300

    Instanciar o Scanner em Sistema

--- src/br/com/zup/SistemaFatura/Sistema.java
+++ src/br/com/zup/SistemaFatura/Sistema.java
@@ -1,4 +1,11 @@
 package br.com.zup.SistemaFatura;
 
+import java.util.Scanner;
+
 public class Sistema {
+
+    private static Scanner capturarDados(String mensagem) {
+        System.out.println(mensagem);
+        return new Scanner(System.in);
+    }
 }
