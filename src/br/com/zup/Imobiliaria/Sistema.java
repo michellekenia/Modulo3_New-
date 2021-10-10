fa0158ba9a222fa7324a3ce31f2c46c376b1e880
Michelle Kênia <keniamsoaress@gmail.com>
2021-10-10 17:43:42 -0300

    Alterar sistema

--- src/br/com/zup/Imobiliaria/Sistema.java
+++ src/br/com/zup/Imobiliaria/Sistema.java
@@ -6,6 +6,9 @@ import java.util.Scanner;
 Todas as classes serão conectadas, e métodos criados para que o programa cumpra a função estabelecida.
  */
 
+// O sistema deve permitir remover um morador de um imóvel a partir do cpf
+// O sistema não pode permitir adicionar um morador com CPF repetido
+
 public class Sistema {
     private static Scanner capturarDados(String mensagem) {
         System.out.println(mensagem);
@@ -19,5 +22,16 @@ public class Sistema {
         System.out.println("Digite 3 para excluir um morador utilizando seu CPF.");
         System.out.println("Digite 4 para sair do sistema");
     }
+
+    public static Morador receberDadosMoradores() {
+        String nome = capturarDados("Digite o nome do morador").nextLine();
+        String cpf = capturarDados("Digite o cpf do morador").nextLine();
+        String telefone = capturarDados("Digite o telefone do morador").nextLine();
+        double renda = capturarDados("Digite a renda do morador").nextDouble();
+
+        Morador morador = new Morador(nome, cpf, telefone, renda);
+        return morador;
+    }
+
 }
 
