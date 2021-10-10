cc844d7d1d5849006e8cdfd4f87486b0042b8d44
Michelle Kênia <keniamsoaress@gmail.com>
2021-10-10 19:17:49 -0300

    Criar metodo Cadastrar Cliente

--- src/br/com/zup/SistemaFatura/Servico_Cliente.java
+++ src/br/com/zup/SistemaFatura/Servico_Cliente.java
@@ -1,4 +1,15 @@
 package br.com.zup.SistemaFatura;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Servico_Cliente {
+
+    List<Cliente> listaDeClientes = new ArrayList<>();
+
+    public void cadastrarCliente( String nome, String cpf, String email) {
+        Cliente cliente = new Cliente(nome, cpf, email);
+        listaDeClientes.add(cliente);
+    }
+
 }
