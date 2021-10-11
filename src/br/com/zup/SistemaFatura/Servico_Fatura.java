b0461afb0db43854a0a01838453630bbaf4693bc
Michelle Kênia <keniamsoaress@gmail.com>
2021-10-10 22:09:41 -0300

    Criar metodo Cadastrar Fatura

--- src/br/com/zup/SistemaFatura/Servico_Fatura.java
+++ src/br/com/zup/SistemaFatura/Servico_Fatura.java
@@ -1,4 +1,15 @@
 package br.com.zup.SistemaFatura;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Servico_Fatura {
+
+    List<Fatura> listaDeFaturas = new ArrayList<>();
+
+    public void cadastrarFatura(double valor, String dataDeVencimento, Cliente cliente) {
+        Fatura fatura = new Fatura(valor, dataDeVencimento, cliente);
+        listaDeFaturas.add(fatura);
+    }
+
 }
